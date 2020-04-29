6edc51e1f39011ae0d0e3b8ae8ecae0587b394fb
PersianBuddy <mnajafi0014@gmail.com>
2020-04-29 13:28:39 +0430

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#! Python 3
+# emailsearcher.py - finds all email addresses
+
+import re
+
+# TODO: create email regular expretion
+
+# TODO: Find matches
+
+# TODO: show mathces or export them in a file
