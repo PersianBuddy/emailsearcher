df049587d588f44d141084d750b2ec9852bc30f8
PersianBuddy <mnajafi0014@gmail.com>
2020-04-30 19:07:42 +0430

    create basic solution

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #! Python 3
-# emailsearcher.py - finds all email addresses
+# main.py - finds all email addresses
 
 import re
+sample_string = 'sdfhk , ckxdf fsds23fm@sd32f.cowefsdfsd3m dskf sdf@gmail.com sfjskdj'
 
 # TODO: create email regular expretion
+phone_reg_ex = re.compile('[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}', re.VERBOSE)
 
 # TODO: Find matches
+result = re.findall(phone_reg_ex,sample_string)
 
 # TODO: show mathces or export them in a file
+print(result)
